92073833e9e547a35c57709c50f5e220887b1825
DulamaA <114867084+DulamaA@users.noreply.github.com>
2023-04-19 13:37:38 +0200

    Added the GitHub repo Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/antonina-dulama-682a2aa5/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Antonina Dulama
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/DulamaA/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
